adcfb380e5ccef8a7ff872a112d751fc8d7ee1f8
Sam Ritchie <sritchie09@gmail.com>
2012-05-15 15:16:18 -0700

    Bump to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twitter/maple "0.1.8"
+(defproject com.twitter/maple "0.1.9-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "All the Cascading taps we have to offer."

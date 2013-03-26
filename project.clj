e1b9aca5719d00da249fff4bbba89b18366259be
Argyris Zymnis <argyris@twitter.com>
2013-03-26 16:32:10 -0700

    Bump to snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twitter/maple "0.2.7"
+(defproject com.twitter/maple "0.2.8-SNAPSHOT"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
   :description "All the Cascading taps we have to offer."

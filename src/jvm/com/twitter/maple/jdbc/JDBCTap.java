a26976a144dc2e591ee6aa012e6ebad76fbabdbe
Eric Theriault <etheriault@apptio.com>
2013-05-15 22:00:00 -0700

    Compilation fix for newer Hadoop versions (?)

--- src/jvm/com/twitter/maple/jdbc/JDBCTap.java
+++ src/jvm/com/twitter/maple/jdbc/JDBCTap.java
@@ -274,7 +274,7 @@ public class JDBCTap extends Tap<JobConf, RecordReader, OutputCollector> {
 
     private JobConf getSourceConf( FlowProcess<JobConf> flowProcess, JobConf conf, String property )
         throws IOException {
-        Map<String, String> priorConf = HadoopUtil.deserializeMapBase64( property, true );
+        Map<String, String> priorConf = HadoopUtil.deserializeBase64( property, conf, Map.class );
         return flowProcess.mergeMapIntoConfig( conf, priorConf );
     }
 

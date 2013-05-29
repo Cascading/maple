135c1dd3b6a3bbaf5a97a3b634fac7500815daac
Eric Y. Theriault <eric@myscenicdrives.com>
2013-05-28 18:04:36 -0700

    Removing unused imports

--- src/jvm/com/twitter/maple/hbase/mapred/TableInputFormat.java
+++ src/jvm/com/twitter/maple/hbase/mapred/TableInputFormat.java
@@ -23,12 +23,10 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.mapred.TableInputFormatBase;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.JobConfigurable;
 import org.apache.hadoop.util.StringUtils;

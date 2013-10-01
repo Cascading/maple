58bd2f8f97e2e8ed35020dc3808146defb82b794
Grant Henke <granthenke@gmail.com>
2013-10-01 08:09:16 -0500

    Add Apache copyright to ported files

--- src/jvm/com/twitter/maple/jdbc/db/BooleanSplitter.java
+++ src/jvm/com/twitter/maple/jdbc/db/BooleanSplitter.java
@@ -10,6 +10,8 @@
  * conditions, unless such conditions are required by law.
  */
 /**
+ * Copyright 2012 The Apache Software Foundation
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information

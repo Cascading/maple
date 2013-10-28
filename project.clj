afdec6a598e04a9641053caeada1252dd6a891a0
Argyris Zymnis <argyris@twitter.com>
2013-10-28 14:22:32 -0700

    Fix build file.

--- project.clj
+++ project.clj
@@ -7,9 +7,8 @@
   :description "All the Cascading taps we have to offer."
   :repositories {"conjars" "http://conjars.org/repo/"}
   :profiles {:provided {:dependencies [[org.apache.hbase/hbase "0.94.5"
-                                        :exclusions [org.apache.hadoop/hadoop-core asm]]]}
-             :dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                                  [asm "3.2"]
-                                  [cascading/cascading-hadoop "2.0.0"
-                                   :exclusions [org.codehaus.janino/janino
-                                                org.apache.hadoop/hadoop-core]]]}})
+                                        :exclusions [org.apache.hadoop/hadoop-core asm]]
+                                        [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                                        [cascading/cascading-hadoop "2.0.0"
+                                         :exclusions [org.codehaus.janino/janino
+                                                      org.apache.hadoop/hadoop-core]]]}})
